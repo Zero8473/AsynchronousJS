8389d57d6dc06714c731e61f690f7de335583058
lisa <lisa@Ubuntu.myguest.virtualbox.org>
2023-10-22 14:54:57 +0200

    code cleanup and restructur

---
+++ oldmaterial.js
@@ -0,0 +1,81 @@
+'use strict';
+
+
+import {renderCountry, countriesContainer, btn} from "./script";
+
+//AJAX Calls
+const getCountryData = function (country) {
+    const request = new XMLHttpRequest();
+    request.open(
+        'GET',
+        `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    );
+    request.send();
+
+    request.addEventListener('load', function () {
+        //[data] is destructured array so = JSON.parse()[0]
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+
+        const html = `
+     <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+    `;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+const getCountryAndNeighbor = function (country) {
+    //AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open(
+        'GET',
+        `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    );
+    request.send();
+
+    request.addEventListener('load', function () {
+        //[data] is destructured array so = JSON.parse()[0]
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        //Render country 1
+        renderCountry(data);
+        //AJAX call neighboring country
+        //Get neighbor country
+        const neighbor = data.borders?.[0];
+
+        if (!neighbor) return;
+
+        const request2 = new XMLHttpRequest();
+        request2.open(
+            'GET',
+            `https://countries-api-836d.onrender.com/countries/alpha/${neighbor}`,
+        );
+        request2.send();
+
+        request2.addEventListener('load', function () {
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
+            renderCountry(data2, 'neighbour');
+        });
+    });
+};
+
+getCountryData('portugal');
+getCountryData('usa');
+getCountryData('germany');
+getCountryData('united kingdom');
+
+//getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
